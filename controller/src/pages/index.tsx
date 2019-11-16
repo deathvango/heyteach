c325c2cf517749bbb40fdbe2b7bca711bbf35afa
Arthur Lisek-Koper <arthurlisekkoper@gmail.com>
2019-11-16 14:16:24 -0600

    Add sample navigation to controller

--- controller/src/pages/index.tsx
+++ controller/src/pages/index.tsx
@@ -1,12 +1,13 @@
-import { NextPage } from 'next';
+import { NextPage } from "next";
+import Link from "next/link";
 
-const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+const Home: NextPage = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <h1>Hello world!</h1>
+  </div>
 );
-
-Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
-  return { userAgent };
-};
 
 export default Home;
