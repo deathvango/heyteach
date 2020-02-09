d72b56adc00b0a1943b5c7bf3b55a3f40ba66874
Arthur Lisek-Koper <arthurlisekkoper@gmail.com>
2020-02-09 15:42:09 -0600

    Add person update mutation

--- api-server/src/graphql/mutations/gqlPersonMutations.ts
+++ api-server/src/graphql/mutations/gqlPersonMutations.ts
@@ -1,26 +1,62 @@
-import { GraphQLFieldConfigMap, GraphQLNonNull, GraphQLString } from "graphql";
+import {
+  GraphQLFieldConfigMap,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLFieldConfigArgumentMap
+} from "graphql";
 import GqlPerson from "../models/gqlPerson";
 import Person from "../../mysql/models/person";
+
+const personArgs: GraphQLFieldConfigArgumentMap = {
+  firstName: {
+    type: new GraphQLNonNull(GraphQLString)
+  },
+  lastName: {
+    type: new GraphQLNonNull(GraphQLString)
+  },
+  email: {
+    type: GraphQLString
+  },
+  phone: {
+    type: GraphQLString
+  }
+};
 
 export const GqlPersonMutations: GraphQLFieldConfigMap<any, any> = {
   addPerson: {
     type: GqlPerson,
     args: {
-      firstName: {
-        type: new GraphQLNonNull(GraphQLString)
-      },
-      lastName: {
-        type: new GraphQLNonNull(GraphQLString)
-      }
+      ...personArgs
     },
     resolve(_, args) {
       return Person.create(<Person>{
         firstName: args.firstName,
-        lastName: args.lastName
+        lastName: args.lastName,
+        email: args.email,
+        phone: args.phone
       });
     }
   },
   updatePerson: {
-    type: GqlPerson
+    type: GqlPerson,
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLString)
+      },
+      ...personArgs
+    },
+    resolve(_, args): Promise<Person> {
+      return Person.findOne<Person>({ where: { id: args.id } }).then(p => {
+        if (p) {
+          return p.update(<Person>{
+            firstName: args.firstName,
+            lastName: args.lastName,
+            email: args.email,
+            phone: args.phone
+          });
+        }
+        return p;
+      });
+    }
   }
 };
